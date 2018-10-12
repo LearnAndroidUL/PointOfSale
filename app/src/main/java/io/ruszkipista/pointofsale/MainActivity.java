fad0f96b8eb9d7a81d1c4a4d64e3401830939cce
Istvan Orosz <16229509@studentmail.ul.ie>
2018-10-12 18:44:23 +0100

    populated menu with items and actions

--- app/src/main/java/io/ruszkipista/pointofsale/MainActivity.java
+++ app/src/main/java/io/ruszkipista/pointofsale/MainActivity.java
@@ -1,6 +1,8 @@
 package io.ruszkipista.pointofsale;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -56,13 +58,15 @@ public class MainActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
+        switch (item.getItemId()){
+            case R.id.action_settings:
+                startActivity(new Intent(Settings.ACTION_LOCALE_SETTINGS));
+                return true;
+            case R.id.action_reset:
+                mCurrentItem = new Item();
+                showCurrentItem();
+                return true;
+        };
         return super.onOptionsItemSelected(item);
     }
 }
