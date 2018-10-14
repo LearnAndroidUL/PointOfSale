b1dc293e89e420e8f42b4be8f205d533eac334b0
ruszkipista <16229509@studentmail.ul.ie>
2018-10-14 15:23:57 +0100

    UI complete non-fuctional

--- app/src/main/java/io/ruszkipista/pointofsale/MainActivity.java
+++ app/src/main/java/io/ruszkipista/pointofsale/MainActivity.java
@@ -1,10 +1,12 @@
 package io.ruszkipista.pointofsale;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -21,11 +23,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // boiler plate code , don't mess with it
+        // boiler plate code, don't mess with it
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-//      grab text viewa of item details
+//      grab text views of item details
         mNameTextView = findViewById(R.id.name_text);
         mQuantityTextView = findViewById(R.id.quantity_text);
         mDateTextView = findViewById((R.id.date_text));
@@ -34,10 +36,23 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mCurrentItem = Item.getDefaultItem();
-                showCurrentItem();
+                addItem();
             }
         });
+    }
+
+    private void addItem() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        View view = getLayoutInflater().inflate(R.layout.dialog_additem,null,false);
+        builder.setView(view);
+        builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                
+            }
+        });
+        builder.setNegativeButton(android.R.string.cancel,null);
+        builder.create().show();
     }
 
     private void showCurrentItem() {
@@ -54,11 +69,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        switch (item.getItemId()){
+        switch (menuItem.getItemId()){
             case R.id.action_reset:
                 final Item mSavedItem = mCurrentItem;
                 mCurrentItem = new Item();
@@ -79,6 +94,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(Settings.ACTION_LOCALE_SETTINGS));
                 return true;
         };
-        return super.onOptionsItemSelected(item);
+        return super.onOptionsItemSelected(menuItem);
     }
 }
